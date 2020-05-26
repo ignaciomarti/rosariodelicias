f67791a367666ac5dd907a5889284fd4ea610c0e
Ignacio <mfignacio97@hotmail.com>
2020-05-25 22:31:58 -0300

    Avances en p

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,105 +1,140 @@
 @extends('front.layouts.app')
 @section('content')
-    <div id="carouselExampleSlidesOnly" class="carousel slide container" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-              <img src="img/foto-welcome1.jpg" class="d-block w-100" alt="Primera foto del carousel">
-          </div>
-          <div class="carousel-item">
-            <img src="img/foto-welcome2.jpg" class="d-block w-100" alt="Segunda foto del carousel">
-          </div>
-          <div class="carousel-item">
-            <img src="img/foto-welcome3.jpg" class="d-block w-100" alt="Tercera foto del carousel">
-          </div>
+
+<!--Carousel Wrapper-->
+<section>
+<div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-2" data-slide-to="1"></li>
+      <li data-target="#carousel-example-2" data-slide-to="2"></li>
+    </ol>
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <div class="view">
+          <img class="d-block w-100" src="img/foto-welcome1.jpg"
+            alt="First slide">
+            <div class="contenedor-titulos">
+              <h1 class="welcomeTitle">Bienvenidos a Rosario Delicias</h1>
+              <h3 class="welcome-title">Lorem ipsum dolor sit.</h3>
+            </div>
+          <div class="mask rgba-black-light"></div>
         </div>
+      </div>
+      <div class="carousel-item">
+        <!--Mask color-->
+        <div class="view">
+          <img class="d-block w-100" src="img/foto-welcome2.jpg"
+            alt="Second slide">
+            <div class="contenedor-titulos">
+              <h1>Los mejores alfajores del mundo</h1>
+              <h3>Amet consectetur adipisicing elit..</h3>
+            </div>
+          <div class="mask rgba-black-strong"></div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <!--Mask color-->
+        <div class="view">
+          <img class="d-block w-100" src="img/foto-welcome3.jpg"
+            alt="Third slide">
+            <div class="contenedor-titulos">
+              <h1>Y alrededores de este mundo</h1>
+              <h3>Sit dolorum ad delectus?</h3>
+            </div>
+          <div class="mask rgba-black-slight"></div>
+        </div>
+      </div>
     </div>
+    <!--/.Slides-->
+    <!--Controls-->
+    <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+    <!--/.Controls-->
+  </div>
+  <!--/.Carousel Wrapper-->
+</section>
 
-    <h2 class="text-center my-4">Productos Destacados</h2>
-        <div class="row justify-content-between container mx-auto">
-
-        <div class="product-card col-3">
-            <div class="product-header">
-                <img src="img/foto-welcome1.jpg" alt="imagen del producto destacado 1">
-            </div><!--product-header-->
-            <div class="product-content">
-                <div class="product-content-header">
-                    <a href="#">
-                        <h3 class="product-title">Alfajor 1</h3>
-                    </a>
-                </div>
-
-                <div class="product-info">
-                    <div class="info-section">
-                        <label>Precio</label>
-                        <span>$100</span>
-                    </div><!--date,time-->
-                    <div class="info-section">
-                        <label>Categoria</label>
-                        <span>Alfajores</span>
-                    </div><!--screen-->
-                    <div class="info-section">
-                        <label>Precio</label>
-                        <span>$100</span>
-                    </div><!--date,time-->
-                </div>
-            </div><!--product-content-->
-        </div><!--product-card-->
-
-        <div class="product-card col-3">
-            <div class="product-header">
-                <img src="img/foto-welcome2.jpg" alt="imagen del producto destacado 1">
-            </div><!--product-header-->
-            <div class="product-content">
-                <div class="product-content-header">
-                    <a href="#">
-                        <h3 class="product-title">Alfajor 2</h3>
-                    </a>
-                </div>
-
-                <div class="product-info">
-                    <div class="info-section">
-                        <label>Precio</label>
-                        <span>$100</span>
-                    </div><!--date,time-->
-                    <div class="info-section">
-                        <label>Categoria</label>
-                        <span>Alfajores</span>
-                    </div><!--screen-->
-                    <div class="info-section">
-                        <label>Precio</label>
-                        <span>$100</span>
-                    </div><!--date,time-->
-                </div>
-            </div><!--product-content-->
-        </div><!--product-card-->
-
-        <div class="product-card col-3">
-            <div class="product-header">
-                <img src="img/foto-welcome3.jpg" alt="imagen del producto destacado 1">
-            </div><!--product-header-->
-            <div class="product-content">
-                <div class="product-content-header">
-                    <a href="#">
-                        <h3 class="product-title">Alfajor 3</h3>
-                    </a>
-                </div>
-
-                <div class="product-info">
-                    <div class="info-section">
-                        <label>Precio</label>
-                        <span>$100</span>
-                    </div><!--date,time-->
-                    <div class="info-section">
-                        <label>Categoria</label>
-                        <span>Alfajores</span>
-                    </div><!--screen-->
-                    <div class="info-section">
-                        <label>Precio</label>
-                        <span>$100</span>
-                    </div><!--date,time-->
-                </div>
-            </div><!--product-content-->
-        </div><!--product-card-->
-
+<section class="container destacados">
+  <h4 class="text-center mt-5 mb-2">Nuestros destacados</h4>
+  <hr>
+  <p class="text-center my-4"> Amamos lo que hacemos</p>
+  <div class="row row-cols-1 row-cols-md-3">
+    <div class="col mb-5">
+      <div class="card">
+        <img src="img/foto-welcome1.jpg" class="card-img-top" alt="Primer producto destacado">
+        <div class="contenedor-descripcion-destacados">
+          <p class="text-center">Producto 1</p>
+          <span class="text-center d-block">Alfajores</span>
+        </div>
+      </div>
     </div>
+    <div class="col mb-5">
+      <div class="card">
+        <img src="img/foto-welcome2.jpg" class="card-img-top" alt="Segundo producto destacado">
+        <div class="contenedor-descripcion-destacados">
+          <p class="text-center">Producto 2</p>
+          <span class="text-center d-block">Alfajores</span>
+        </div>
+      </div>
+    </div>
+    <div class="col mb-5">
+      <div class="card">
+        <img src="img/foto-welcome3.jpg" class="card-img-top" alt="Tercer producto destacado">
+        <div class="contenedor-descripcion-destacados">
+          <p class="text-center">Producto 3</p>
+          <span class="text-center d-block">Alfajores</span>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="img/foto-welcome3.jpg" class="card-img-top" alt="Cuarto producto destacado">
+        <div class="contenedor-descripcion-destacados">
+          <p class="text-center">Producto 4</p>
+          <span class="text-center d-block">Alfajores</span>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="img/foto-welcome2.jpg" class="card-img-top" alt="Quinto producto destacado">
+        <div class="contenedor-descripcion-destacados">
+          <p class="text-center">Producto 5</p>
+          <span class="text-center d-block">Alfajores</span>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="img/foto-welcome1.jpg" class="card-img-top" alt="Sexto producto destacado">
+        <div class="contenedor-descripcion-destacados">
+          <p class="text-center">Producto 6</p>
+          <span class="text-center d-block">Alfajores</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <button class="mx-auto d-block mt-5">Ver todos los productos</button>
+</section>
+
+<section class="testimoniales d-flex justify-content-between align-items-center my-5">
+  <img src="img/panadero.jpg" alt="imagen de Florian Martí">
+  <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, velit. adipisicing elit. Id, velit.</blockquote>
+</section>
+
+<section class="diferencia">
+  <h4 class="text-center mt-5 mb-2">Lo que nos diferencia</h4>
+  <hr>
+  <p class="text-center my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit?</p>
+</section>
 @endsection
--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -7,49 +7,39 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Rosario Delicias') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app2.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <script src="https://kit.fontawesome.com/20ee0b6ed7.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"> 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="container-fluid" id="app">
-        <nav class="container navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Rosario Delicias</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item active mr-3">
-                  <a class="nav-link" href="/">Inicio</a>
-                </li>
-                <li class="nav-item active mr-3">
-                <a class="nav-link" href="/productos">Productos</a>
-                </li>
-                <li class="nav-item active mr-3">
-                  <a class="nav-link" href="/nosotros">Nosotros</a>
-                </li>
-                <li class="nav-item active">
-                  <a class="nav-link" href="/contacto">Contacto</a>
-                </li>
-              </ul>
-            </div>
+      <header class="bg-white">
+        <nav class="d-flex justify-content-between align-items-center container">
+          <a href="#" class="px-3 py-5">Empresa</a>
+          <a href="#" class="px-3 py-5">Productos</a>
+          <a href="#" class="px-3 py-5">Categorías</a>
+          <img src="img/logo.jpg" class="logo" alt="logo de Rosario Delicias">
+          <a href="#" class="px-3 py-5">Redes</a>
+          <a href="#" class="px-3 py-5">Contacto</a>
+          <a href="#" class="px-3 py-5">Carrito</a>
         </nav>
-
+      </header>
 
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
+
     @include('front.layouts.footer')
 </body>
 </html>
